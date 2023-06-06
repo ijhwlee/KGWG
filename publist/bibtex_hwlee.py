743462f6bc2fb6cdc287833b124f426ffe8fe938
ijhwlee <hwlee@inje.ac.kr>
2023-06-07 01:13:34 +0900

    modify bug for @ symbol within the title include includeHtml.js file

--- publist/bibtex_hwlee.py
+++ publist/bibtex_hwlee.py
@@ -38,16 +38,50 @@ def generate_list_item(text_file, bibitem, bibcode):
   text_file.write("<a href='https://ui.adsabs.harvard.edu/#abs/{0}/abstract'>{1}</a>\n".format(bibcode, get_title(bibitem)))
   text_file.write("</li>\n")
 
-def generate_html(year, bibitems, bibcodes):
-  html_file = "kgwg_publications_"+str(year)+".html"
+def generate_ol(year, bibitems, bibcodes):
+  html_file = "kgwg_publications_"+str(year)+"_ol.html"
   with open(html_file, "w") as text_file:
-    text_file.write("<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>Year {0}: List of Publications for KGWG</title></head>\n".format(year))
-    text_file.write("<body>\n<h1>List of Publications for KGWG in year {0}</h1>\n".format(year))
     text_file.write("<ol>\n")
     for idx in range(len(bibitems)):
       generate_list_item(text_file, bibitems[idx], bibcodes[idx])
     text_file.write("</ol>\n")
+
+def generate_html(year, bibitems, bibcodes):
+  html_file = "kgwg_publications_"+str(year)+".html"
+  ol_file = "kgwg_publications_"+str(year)+"_ol.html"
+  with open(html_file, "w") as text_file:
+    text_file.write("<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>Year {0}: List of Publications for KGWG</title></head>\n".format(year))
+    # writing script
+    text_file.write("<script type=\"text/javascript\" src=\"./includeHtml.js\"></script>\n")
+    text_file.write("</head>\n")
+    text_file.write("<body>\n<h1>List of Publications for KGWG in year {0}</h1>\n".format(year))
+    #text_file.write("<ol>\n")
+    #for idx in range(len(bibitems)):
+    #  generate_list_item(text_file, bibitems[idx], bibcodes[idx])
+    #text_file.write("</ol>\n")
+    text_file.write("<div w3-include-html=\"./{0}\"></div>\n".format(ol_file))
+    text_file.write("<script>includeHTML();</script>\n")
     text_file.write("</body>\n")
+  generate_ol(year, bibitems, bibcodes)
+
+def get_bibitem_list(bibitems):
+  #bibitems1 = bibitems.split('@')
+  bibitems1 = []
+  bib_lines = bibitems.split('\n')
+  bibitem = ""
+  for idx in range(len(bib_lines)):
+    this_line = bib_lines[idx]
+    if this_line.startswith('@'):
+      if bibitem.startswith('@'):
+        bibitem += "\n"
+        bibitems1.append(bibitem)
+      bibitem = this_line
+    elif len(this_line)>0:
+      bibitem += "\n"+this_line
+  if bibitem.startswith('@'):
+    bibitem += "\n"
+    bibitems1.append(bibitem)
+  return bibitems1
 
 def get_bibcodes(author, year):
   url = "https://api.adsabs.harvard.edu/v1/search/query?q=(%3Dauthor%3A%22"+author+"%22%20AND%20year%3A"+str(year)+")&fl=bibcode&rows=200&sort=date%20desc%2C%20bibcode%20desc"
@@ -98,9 +132,16 @@ def get_bibitems_year(file_name, year):
   print("     Total {0} bibitems for year {1}".format(len(bibcodes), year))
   print("=====================================================================================================")
   bibitems = bibtex.json()['export']
-  bibitems1 = bibitems.split('@')
+  #bibitems1 = bibitems.split('@')
+  bibitems1 = get_bibitem_list(bibitems)
   bibitems2 = [bibitem for bibitem in bibitems1 if len(bibitem) > 0]
-  print("len(bibitems) = {0}, len(bibitems2) = {1}, len(bibcodes) = {2}".format(len(bibitems), len(bibitems2), len(bibcodes)))
+  print("len(bibitems) = {0}, len(bibitems) = {1}, len(bibitems2) = {2}, len(bibcodes) = {3}".format(len(bibitems), len(bibitems1), len(bibitems2), len(bibcodes)))
+  #print("==== Bibitems ==========\n")
+  #for idx in range(len(bibitems2)):
+  #  print("bibitems2[{0}] :{1}\n".format(idx, bibitems2[idx]))
+  #print("==== Bibcodes ==========\n")
+  #for idx in range(len(bibcodes)):
+  #  print("bibcodes[{0}] :{1}\n".format(idx, bibcodes[idx]))
   file_name = "kgwg_publications_"+str(year)+"_"+current_month_text+".bib"
   with open(file_name, "w") as text_file:
     text_file.write("{0}".format(bibitems))
