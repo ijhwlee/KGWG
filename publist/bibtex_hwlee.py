3518682f092187497a711e6acae14c2cf7b919f0
ijhwlee <hwlee@inje.ac.kr>
2023-06-08 21:17:41 +0900

    hide ADS API key from source

--- publist/bibtex_hwlee.py
+++ publist/bibtex_hwlee.py
@@ -7,7 +7,18 @@ if len(sys.argv) > 1 and not sys.argv[1].isnumeric():
   print("Usage: python3 {0} yyyy [file_name]\n       Second argument should be a number(zero for the current year).".format(sys.argv[0]))
   exit(-1)
 
-token="wDu6gqEIMIwejzfACae7mIrAeWp9GniUMTe4WP58"
+ads_key_file_name = ".ads_key.dat"
+if os.path.exists(ads_key_file_name):
+  pass
+else:
+  print("ERROR- ADS API key file {0} does not exist, please specify a correct file name.".format(ads_key_file_name))
+  exit(-1)
+with open(ads_key_file_name, "r") as key_file:
+  lines = key_file.read()
+  keys = lines.split('\n')
+token = keys[0]
+print("[DEBUG-hwlee]token = {0}".format(token))
+
 current_year = datetime.now().year
 if len(sys.argv) > 1:
   current_year = int(sys.argv[1])
@@ -28,14 +39,77 @@ def get_title(bibitem):
   lines = bibitem.split('\n')
   for line in lines:
     if 'title' in line:
-      start = line.find('{')
-      end = line.find('}')
-      title = line[start+1: end]
+      start = line.find('"{')
+      end = line.find('}",$')
+      title = line[start+2: end-2]
+      print("Title = {0}".format(line))
+      print("Extracted = {0}".format(title))
+      return title
+
+def get_author(bibitem):
+  lines = bibitem.split('\n')
+  for line in lines:
+    if 'author' in line:
+      start = line.find('= {')
+      end = line.find('},$')
+      title = line[start+3: end-1]
+      print("Author before : {0}".format(title))
+      title.replace("{", "")
+      title.replace("}", "")
+      print("Author after : {0}".format(title))
+      return title
+
+def convert_journal(journal):
+  if '\\prd' in journal:
+    return "Phys. Rev. D"
+  elif '\\prl' in journal:
+    return "Phys. Rev. Lett."
+  elif '\\prx' in journal:
+    return "Phys. Rev. X"
+  elif '\\apj' in journal:
+    return "Astro. Phys. J."
+  elif '\\apjl' in journal:
+    return "Astro. Phys. J. Lett."
+  elif '\\apjs' in journal:
+    return "Astro. Phys. J. Supp."
+  elif '\\aap' in journal:
+    return "Astro. Astrophys."
+  elif '\\mnras' in journal:
+    return "Mon. Not. Roy. Astro. Soc."
+  else:
+    return journal
+
+def get_journal(bibitem):
+  lines = bibitem.split('\n')
+  ref_type = ""
+  for line in lines:
+    if 'ARTICLE' in line:
+      ref_type = 'ARTICLE'
+    elif 'BOOK' in line:
+      ref_type = 'BOOK'
+    elif 'INPROCEEDINGS' in line:
+      ref_type = 'INPROCEEDINGS'
+
+    if ref_type=='ARTICLE' and 'journal' in line:
+      start = line.find('= {')
+      end = line.find('},$')
+      title = line[start+3: end-1]
+      title = convert_journal(title)
+      return title
+    if ref_type=='BOOK' and 'title' in line:
+      start = line.find('"{')
+      end = line.find('}",$')
+      title = line[start+2: end-2]
+      return title
+    if ref_type=='INPROCEEDINGS' and 'booktitle' in line:
+      start = line.find('= {')
+      end = line.find('},$')
+      title = line[start+3: end-1]
       return title
 
 def generate_list_item(text_file, bibitem, bibcode):
   text_file.write("<li>")
-  text_file.write("<a href='https://ui.adsabs.harvard.edu/#abs/{0}/abstract'>{1}</a>\n".format(bibcode, get_title(bibitem)))
+  text_file.write("<a href='https://ui.adsabs.harvard.edu/#abs/{0}/abstract' target='_blank'>{1}</a>\n".format(bibcode, get_title(bibitem)))
   text_file.write("</li>\n")
 
 def generate_ol(year, bibitems, bibcodes):
@@ -105,6 +179,24 @@ def get_authors(file_name):
   authors0 = [author for author in authors if len(author)>0]
   return authors0
 
+def is_full_author(bibitem):
+  is_full = False
+  author = get_author(bibitem)
+  if 'Abbott' in author and 'et al.' in author:
+    is_full = True
+  print("Author = {0}, Full_author = {1}".format(author, is_full))
+  return is_full
+
+def get_full_short_authors(bibitems):
+  bibitem_short = []
+  bibitem_full = []
+  for idx in range(len(bibitems)):
+    if is_full_author(bibitems[idx]):
+      bibitem_full.append(bibitems[idx])
+    else:
+      bibitem_short.append(bibitems[idx])
+  return bibitem_full, bibitem_short
+
 def get_bibitems_year(file_name, year):
   #url = "https://api.adsabs.harvard.edu/v1/search/query?q=((%3Dauthor%3A%22Lee,H.M.%22%20or%20%3Dauthor%3A%22Lee,Hyung%20Mok%22)%20AND%20year%3A"+str(year)+")&fl=bibcode&rows=200&sort=date%20desc%2C%20bibcode%20desc"
   #query is https://api.adsabs.harvard.edu/v1/search/query?q=((=author:"Lee, H.M." or =author:"Lee, Hyung Mok") and year:2022)&fl=bibcode&rows=200&sort=date+desc,bibcode+desc
@@ -136,6 +228,8 @@ def get_bibitems_year(file_name, year):
   bibitems1 = get_bibitem_list(bibitems)
   bibitems2 = [bibitem for bibitem in bibitems1 if len(bibitem) > 0]
   print("len(bibitems) = {0}, len(bibitems) = {1}, len(bibitems2) = {2}, len(bibcodes) = {3}".format(len(bibitems), len(bibitems1), len(bibitems2), len(bibcodes)))
+  bibitem_full, bibitem_short = get_full_short_authors(bibitems2)
+  print("len(bibitem_full) = {0}, len(bibitem_short) = {1}".format(len(bibitem_full), len(bibitem_short)))
   #print("==== Bibitems ==========\n")
   #for idx in range(len(bibitems2)):
   #  print("bibitems2[{0}] :{1}\n".format(idx, bibitems2[idx]))
