6c9973ae792b25fd0739e0a7eb0c9b0b7948ef8e
ijhwlee <hwlee@inje.ac.kr>
2023-06-09 00:32:17 +0900

    change the publication list staring authors and separate collaboration and short author

--- publist/bibtex_hwlee.py
+++ publist/bibtex_hwlee.py
@@ -3,6 +3,8 @@ import os
 import requests
 from datetime import datetime
 
+MIN_AUTHORS_COLL = 15
+
 if len(sys.argv) > 1 and not sys.argv[1].isnumeric():
   print("Usage: python3 {0} yyyy [file_name]\n       Second argument should be a number(zero for the current year).".format(sys.argv[0]))
   exit(-1)
@@ -17,7 +19,7 @@ with open(ads_key_file_name, "r") as key_file:
   lines = key_file.read()
   keys = lines.split('\n')
 token = keys[0]
-print("[DEBUG-hwlee]token = {0}".format(token))
+#print("[DEBUG-hwlee]token = {0}".format(token))
 
 current_year = datetime.now().year
 if len(sys.argv) > 1:
@@ -42,8 +44,8 @@ def get_title(bibitem):
       start = line.find('"{')
       end = line.find('}",$')
       title = line[start+2: end-2]
-      print("Title = {0}".format(line))
-      print("Extracted = {0}".format(title))
+      #print("Title = {0}".format(line))
+      #print("Extracted = {0}".format(title))
       return title
 
 def get_author(bibitem):
@@ -53,11 +55,63 @@ def get_author(bibitem):
       start = line.find('= {')
       end = line.find('},$')
       title = line[start+3: end-1]
-      print("Author before : {0}".format(title))
-      title.replace("{", "")
-      title.replace("}", "")
-      print("Author after : {0}".format(title))
+      #print("Author before : {0}".format(title))
+      title = title[1:]
+      title = title.replace(" {", " ")
+      title = title.replace("},", ",")
+      title = title.strip()
+      #print("Author after : {0}".format(title))
       return title
+
+def get_volume(bibitem):
+  title = ""
+  lines = bibitem.split('\n')
+  for line in lines:
+    if 'volume' in line:
+      start = line.find('= {')
+      end = line.find('},$')
+      title = line[start+3: end-1]
+      #print("Volume : {0}".format(title))
+      return title
+  return title; # not exist volume PROCEEDINGS case
+
+def get_adsurl(bibitem):
+  title = ""
+  lines = bibitem.split('\n')
+  for line in lines:
+    if 'adsurl' in line:
+      start = line.find('= {')
+      end = line.find('},$')
+      #print("adsurl : {0}".format(line))
+      title = line[start+3: end-1]
+      #print("adsurl extracted : {0}".format(title))
+      return title
+  return title; # not exist volume PROCEEDINGS case
+
+def get_year(bibitem):
+  title = ""
+  lines = bibitem.split('\n')
+  for line in lines:
+    if 'year' in line:
+      start = line.find('= ')
+      end = line.find(',$')
+      title = line[start+2: end]
+      #print("Year : {0}".format(title))
+      return title
+  return title; # not exist year not proper bibitem
+
+def get_pages(bibitem):
+  title = ""
+  lines = bibitem.split('\n')
+  for line in lines:
+    if 'pages' in line:
+      start = line.find('= {')
+      end = line.find('},$')
+      #print("Pages : {0}".format(line))
+      title = line[start+3: end-1]
+      #print("Pages extracted : {0}".format(title))
+      return title
+  return title; # not exist pages
 
 def convert_journal(journal):
   if '\\prd' in journal:
@@ -109,7 +163,12 @@ def get_journal(bibitem):
 
 def generate_list_item(text_file, bibitem, bibcode):
   text_file.write("<li>")
-  text_file.write("<a href='https://ui.adsabs.harvard.edu/#abs/{0}/abstract' target='_blank'>{1}</a>\n".format(bibcode, get_title(bibitem)))
+  #text_file.write("<a href='https://ui.adsabs.harvard.edu/#abs/{0}/abstract' target='_blank'>{1}</a>\n".format(bibcode, get_title(bibitem)))
+  adsurl = get_adsurl(bibitem)
+  if adsurl == "":
+    text_file.write("<a href='https://ui.adsabs.harvard.edu/#abs/{0}/abstract' target='_blank'>{1}</a>\n".format(bibcode, get_title(bibitem)))
+  else:
+    text_file.write("<a href='{0}' target='_blank'>{1}</a>\n".format(adsurl, mk_link_string(bibitem, True)))
   text_file.write("</li>\n")
 
 def generate_ol(year, bibitems, bibcodes):
@@ -120,7 +179,24 @@ def generate_ol(year, bibitems, bibcodes):
       generate_list_item(text_file, bibitems[idx], bibcodes[idx])
     text_file.write("</ol>\n")
 
-def generate_html(year, bibitems, bibcodes):
+def generate_full(year, bibitems, bibcodes):
+  html_file = "kgwg_publications_"+str(year)+"_full.html"
+  with open(html_file, "w") as text_file:
+    text_file.write("<ol>\n")
+    for idx in range(len(bibitems)):
+      generate_list_item(text_file, bibitems[idx], bibcodes[idx])
+    text_file.write("</ol>\n")
+
+def generate_short(year, bibitems, bibcodes):
+  html_file = "kgwg_publications_"+str(year)+"_short.html"
+  with open(html_file, "w") as text_file:
+    text_file.write("<ol>\n")
+    for idx in range(len(bibitems)):
+      generate_list_item(text_file, bibitems[idx], bibcodes[idx])
+    text_file.write("</ol>\n")
+
+#def generate_html(year, bibitems, bibcodes):
+def generate_html(year, bibitems_full, bibcodes_full, bibitems_short, bibcodes_short):
   html_file = "kgwg_publications_"+str(year)+".html"
   ol_file = "kgwg_publications_"+str(year)+"_ol.html"
   with open(html_file, "w") as text_file:
@@ -136,7 +212,9 @@ def generate_html(year, bibitems, bibcodes):
     text_file.write("<div w3-include-html=\"./{0}\"></div>\n".format(ol_file))
     text_file.write("<script>includeHTML();</script>\n")
     text_file.write("</body>\n")
-  generate_ol(year, bibitems, bibcodes)
+  #generate_ol(year, bibitems, bibcodes)
+  generate_full(year, bibitems_full, bibcodes_full)
+  generate_short(year, bibitems_short, bibcodes_short)
 
 def get_bibitem_list(bibitems):
   #bibitems1 = bibitems.split('@')
@@ -179,23 +257,70 @@ def get_authors(file_name):
   authors0 = [author for author in authors if len(author)>0]
   return authors0
 
+def count_authors(author):
+  authors = author.split('and')
+  count = len(authors)
+  return count
+
+def mk_short_list(author):
+  authors = author.split('and')
+  short_list = ""
+  for idx in range(4):
+    short_list += authors[idx] + " and "
+  short_list += "et al."
+  return short_list
+
 def is_full_author(bibitem):
   is_full = False
   author = get_author(bibitem)
-  if 'Abbott' in author and 'et al.' in author:
+  count = count_authors(author)
+  if 'et al.' in author or count > MIN_AUTHORS_COLL:
     is_full = True
-  print("Author = {0}, Full_author = {1}".format(author, is_full))
+  #print("Author = {0}, Full_author = {1}, Count = {2}".format(author, is_full, count))
   return is_full
 
-def get_full_short_authors(bibitems):
+def get_full_short_authors(bibitems, bibcodes):
   bibitem_short = []
   bibitem_full = []
+  bibcodes_short = []
+  bibcodes_full = []
+  if len(bibitems) != len(bibcodes) :
+    print("ERROR: len(bibitems) = {0} is not equal to len(bibcodes) = {1} in get_full_short_authors().".format(len(bibitems), len(bibcodesa)))
+    return bibitem_full, bibcodes_full, bibitem_short, bibcodes_short
+    
   for idx in range(len(bibitems)):
     if is_full_author(bibitems[idx]):
       bibitem_full.append(bibitems[idx])
+      bibcodes_full.append(bibcodes[idx])
     else:
       bibitem_short.append(bibitems[idx])
-  return bibitem_full, bibitem_short
+      bibcodes_short.append(bibcodes[idx])
+  return bibitem_full, bibcodes_full, bibitem_short, bibcodes_short
+
+def mk_link_string(bibitem, add_title):
+  link_string = ""
+  author = get_author(bibitem)
+  count = count_authors(author)
+  if count > MIN_AUTHORS_COLL:
+    author = mk_short_list(author)
+  journal = get_journal(bibitem)
+  volume = get_volume(bibitem)
+  pages = get_pages(bibitem)
+  year = get_year(bibitem)
+  link_string = author
+  if len(journal) > 0:
+    link_string += ", "+journal
+  if len(volume) > 0:
+    link_string += " "+volume
+  if len(pages) > 0:
+    link_string += ", "+pages
+  if len(year) > 0:
+    link_string += "("+year+")"
+  if add_title:
+    title = get_title(bibitem)
+    if len(title) > 0:
+      link_string += ", \""+title+"\""
+  return link_string
 
 def get_bibitems_year(file_name, year):
   #url = "https://api.adsabs.harvard.edu/v1/search/query?q=((%3Dauthor%3A%22Lee,H.M.%22%20or%20%3Dauthor%3A%22Lee,Hyung%20Mok%22)%20AND%20year%3A"+str(year)+")&fl=bibcode&rows=200&sort=date%20desc%2C%20bibcode%20desc"
@@ -228,8 +353,8 @@ def get_bibitems_year(file_name, year):
   bibitems1 = get_bibitem_list(bibitems)
   bibitems2 = [bibitem for bibitem in bibitems1 if len(bibitem) > 0]
   print("len(bibitems) = {0}, len(bibitems) = {1}, len(bibitems2) = {2}, len(bibcodes) = {3}".format(len(bibitems), len(bibitems1), len(bibitems2), len(bibcodes)))
-  bibitem_full, bibitem_short = get_full_short_authors(bibitems2)
-  print("len(bibitem_full) = {0}, len(bibitem_short) = {1}".format(len(bibitem_full), len(bibitem_short)))
+  bibitem_full, bibcodes_full, bibitem_short, bibcodes_short = get_full_short_authors(bibitems2, bibcodes)
+  print("len(bibitem_full) = {0}, len(bibcodes_full) = {1}, len(bibitem_short) = {2}, len(bibcodes_short) = {3}".format(len(bibitem_full), len(bibcodes_full), len(bibitem_short), len(bibcodes_short)))
   #print("==== Bibitems ==========\n")
   #for idx in range(len(bibitems2)):
   #  print("bibitems2[{0}] :{1}\n".format(idx, bibitems2[idx]))
@@ -239,7 +364,8 @@ def get_bibitems_year(file_name, year):
   file_name = "kgwg_publications_"+str(year)+"_"+current_month_text+".bib"
   with open(file_name, "w") as text_file:
     text_file.write("{0}".format(bibitems))
-  generate_html(year, bibitems2, bibcodes)
+  #generate_html(year, bibitems2, bibcodes)
+  generate_html(year, bibitem_full, bibcodes_full, bibitem_short, bibcodes_short)
 
 for year in range(current_year - 2, current_year+1):
   get_bibitems_year(author_file_name, year)
--- publist/kgwg/kgwg_functions/kgwg_functions.php
+++ publist/kgwg/kgwg_functions/kgwg_functions.php
@@ -31,7 +31,8 @@ function wpb_kgwg_list($attrs) {
 add_shortcode('mklist', 'wpb_kgwg_list');
 
 function wpb_kgwg_publications($attrs) {
-  $file_name = './publications/kgwg_publications_'.$attrs['year'].'_ol.html';
+  //$file_name = './publications/kgwg_publications_'.$attrs['year'].'_ol.html';
+  $file_name = './publications/kgwg_publications_'.$attrs['year'].'_full.html';
   $myfile = fopen($file_name, "r");
   if (!$myfile) {
     $message = "Unable to open file [".$file_name."]!";
@@ -39,16 +40,20 @@ function wpb_kgwg_publications($attrs) {
   }
   $content = fread($myfile,filesize($file_name));
   fclose($myfile);
-  //$num = 6;
-  //$delta = 2;
-  $message = '<h1>Full Author Publications</h1>';
+  $message = '<h1>Collaboration Papers</h1>';
   $message .= $content;
-  //$message .= '<ol>';
-  //$message .= $file_name;
-  //for ($x=0; $x <= $num; $x += $delta) {
-  //  $message .= '<li><a href="https://ui.adsabs.harvard.edu/#abs/2018mgm..conf.3170O/abstract" target="_blank">Development of KAGRA Algorithmic Library (KAGALI)</a></li>';
-  //}
-  //$message .= '</ol>';
+
+  $file_name = './publications/kgwg_publications_'.$attrs['year'].'_short.html';
+  $myfile = fopen($file_name, "r");
+  if (!$myfile) {
+    $message = "Unable to open file [".$file_name."]!";
+    return $message;
+  }
+  $content = fread($myfile,filesize($file_name));
+  fclose($myfile);
+  $message .= '<h1>Short Author Papers</h1>';
+  $message .= $content;
+
   return $message;
 }
 add_shortcode('publications', 'wpb_kgwg_publications');
