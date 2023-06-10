f018eb42237b8781fd70920d372083318e2b5d15
ijhwlee <hwlee@inje.ac.kr>
2023-06-10 16:13:34 +0900

    make automation for publication list

--- publist/kgwg/kgwg_functions/kgwg_functions.php
+++ publist/kgwg/kgwg_functions/kgwg_functions.php
@@ -32,10 +32,13 @@ add_shortcode('mklist', 'wpb_kgwg_list');
 
 function wpb_kgwg_publications($attrs) {
   //$file_name = './publications/kgwg_publications_'.$attrs['year'].'_ol.html';
-  $file_name = './publications/kgwg_publications_'.$attrs['year'].'_full.html';
+  //$pub_year = $attrs['year'];
+  $pub_year = wpb_kgwg_pub_year();
+  $file_name = './publications/kgwg_publications_'.$pub_year.'_full.html';
   $myfile = fopen($file_name, "r");
   if (!$myfile) {
-    $message = "Unable to open file [".$file_name."]!";
+    $message = "<h1>Not available publication list for Year ".$pub_year.".</h1><hr> Unable to open file [".$file_name."]!<br>";
+    $message .= "You may need to run /publications/mk_pub_list.sh script.";
     return $message;
   }
   $content = fread($myfile,filesize($file_name));
@@ -43,7 +46,7 @@ function wpb_kgwg_publications($attrs) {
   $message = '<h1>Collaboration Papers</h1>';
   $message .= $content;
 
-  $file_name = './publications/kgwg_publications_'.$attrs['year'].'_short.html';
+  $file_name = './publications/kgwg_publications_'.$pub_year.'_short.html';
   $myfile = fopen($file_name, "r");
   if (!$myfile) {
     $message = "Unable to open file [".$file_name."]!";
@@ -57,3 +60,50 @@ function wpb_kgwg_publications($attrs) {
   return $message;
 }
 add_shortcode('publications', 'wpb_kgwg_publications');
+
+function wpb_kgwg_get_query() {
+  //$queries = array();
+  //parse_str($_SERVER['QUERY_STRING'], $queries);
+  //$size = sizeof($queries);
+  //$message = "query size : ".$size;
+  //for ($n=0; $n < $size; $n++) {
+  //  $message .= queries[$n];
+  //}
+  $message = "";
+  $year = "";
+  if ( isset( $_GET['pub'] )) {
+    $year .= $_GET['pub'];
+    $message .= "Year = ".$year;
+  }
+  return $message;
+}
+add_shortcode('get_query', 'wpb_kgwg_get_query');
+
+function wpb_kgwg_list_years($attrs) {
+  $s_year = $attrs['start'];
+  $e_year = $attrs['end'];
+  $message = "<ul>";
+  for ($y=(int)$s_year; $y <= (int)$e_year; $y++) {
+    $message .= "<li><a href=\"/list-of-publications-for-kgwg-in-year-2018/?pub_year=".$y."\">Year ".$y."</a></li>";
+  }
+  $message .= "</ul>";
+  return $message;
+}
+add_shortcode('list_years', 'wpb_kgwg_list_years');
+
+function wpb_kgwg_pub_year() {
+  $year = "";
+  if ( isset( $_GET['pub_year'] )) {
+    $year .= $_GET['pub_year'];
+  }
+  return $year;
+}
+add_shortcode('pub_year', 'wpb_kgwg_pub_year');
+
+function wpb_kgwg_change_title() {
+  $year = wpb_kgwg_pub_year();
+  $message = '<script>document.getElementById("intro-core-wrap").innerHTML="<h1 class=\"page-title\">List of Publications for KGWG in year '.$year.'</h1>";</script>';
+  return $message;
+}
+add_shortcode('change_title', 'wpb_kgwg_change_title');
+
