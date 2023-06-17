324e02065e92d51c875ef21bdb9dc493d9174584
ijhwlee <hwlee@inje.ac.kr>
2023-06-17 10:18:08 +0900

    finalize kgwg functions

--- publist/kgwg/kgwg_functions/kgwg_functions.php
+++ publist/kgwg/kgwg_functions/kgwg_functions.php
@@ -33,29 +33,48 @@ add_shortcode('mklist', 'wpb_kgwg_list');
 function wpb_kgwg_publications($attrs) {
   //$file_name = './publications/kgwg_publications_'.$attrs['year'].'_ol.html';
   //$pub_year = $attrs['year'];
+  $no_short = True;
   $pub_year = wpb_kgwg_pub_year();
   $file_name = './publications/kgwg_publications_'.$pub_year.'_full.html';
   $myfile = fopen($file_name, "r");
   if (!$myfile) {
-    $message = "<h1>Not available publication list for Year ".$pub_year.".</h1><hr> Unable to open file [".$file_name."]!<br>";
-    $message .= "You may need to run /publications/mk_pub_list.sh script.";
-    return $message;
+    $message = "<h1>Not available collaboration publication list for Year ".$pub_year.".</h1><hr> Unable to open file [".$file_name."]!<br>";
+    if ($no_short) {
+      $message .= "You may need to run /publications/mk_pub_list.sh script.";
+      return $message;
+    }
   }
-  $content = fread($myfile,filesize($file_name));
-  fclose($myfile);
-  $message = '<h1>Collaboration Papers</h1>';
-  $message .= $content;
+  else {
+    $content = fread($myfile,filesize($file_name));
+    fclose($myfile);
+    $message = '<h1>Collaboration Papers</h1>';
+    $message .= $content;
+    //$message .= "<div class=\"wp-block-file\"><a href=\"https://www2.kgwg.org/publications/kgwg_publications_".$pub_year."_Jun_full.bib\" class=\"wp-block-file__button wp-element-button\" download>Downlaod bib</a></div>";
+    if ($no_short) {
+      return $message;
+    }
+  }
 
-  $file_name = './publications/kgwg_publications_'.$pub_year.'_short.html';
-  $myfile = fopen($file_name, "r");
-  if (!$myfile) {
-    $message = "Unable to open file [".$file_name."]!";
-    return $message;
+  // set the following variable to be True, if you want to separated short author paper list
+  $separated = False;
+  if (! $separated) {
+  // list in the same page
+    $file_name = './publications/kgwg_publications_'.$pub_year.'_short.html';
+    $myfile = fopen($file_name, "r");
+    if (!$myfile) {
+      $message .= "Unable to open file [".$file_name."]!<br>";
+      $message .= "You may need to run /publications/mk_pub_list.sh script.";
+      return $message;
+    }
+    $content = fread($myfile,filesize($file_name));
+    fclose($myfile);
+    $message .= '<h1>GW Short-authored Papers</h1>';
+    $message .= $content;
   }
-  $content = fread($myfile,filesize($file_name));
-  fclose($myfile);
-  $message .= '<h1>Short Author Papers</h1>';
-  $message .= $content;
+  else {
+  // using separate short author pages
+    $message .= '<h1><a href="https://www2.kgwg.org/gw-short-authored-papers/?pub_year='.$pub_year.'">GW Short-authored Paper List</a></h1>';
+  }
 
   return $message;
 }
@@ -94,14 +113,17 @@ function wpb_kgwg_list_years($attrs) {
   //$message .= "</ul>";
   // using table
   $columns = 8;
-  $message = "<table class='table table-striped'> <tbody>";
+  //$message = "<table class='pub_years'> <tbody>";
+  $message = "<table id='customers'> <tbody>";
   $count = 0;
   for ($y=(int)$s_year; $y <= (int)$e_year; $y++) {
     if ($count==0) {
-      $message .= "<tr class='table-secondary'>";
+      //$message .= "<tr class='pub_years pub_tr'>";
+      $message .= "<tr>";
     }
     else if ($count%$columns == 0 and $y < (int)$e_year) {
-      $message .= "</tr><tr class='table-secondary'>";
+      //$message .= "</tr><tr class='pub_years pub_tr'>";
+      $message .= "</tr><tr>";
     }
     else if ($count%$columns == 0) {
       $message .= "</tr>";
@@ -126,9 +148,22 @@ function wpb_kgwg_pub_year() {
 }
 add_shortcode('pub_year', 'wpb_kgwg_pub_year');
 
+function wpb_kgwg_change_title_short() {
+  $year = wpb_kgwg_pub_year();
+  if ($year == "") {
+    $year = date("Y");
+  }
+  $message = '<script>document.getElementById("intro-core-wrap").innerHTML="<h1 class=\"page-title\">List of GW Short-authored Papers for KGWG in year '.$year.'</h1>";</script>';
+  return $message;
+}
+add_shortcode('change_title_short', 'wpb_kgwg_change_title_short');
+
 function wpb_kgwg_change_title() {
   $year = wpb_kgwg_pub_year();
-  $message = '<script>document.getElementById("intro-core-wrap").innerHTML="<h1 class=\"page-title\">List of Publications for KGWG in year '.$year.'</h1>";</script>';
+  if ($year == "") {
+    $year = date("Y");
+  }
+  $message = '<script>document.getElementById("intro-core-wrap").innerHTML="<h1 class=\"page-title\">List of Collaboration Papers for KGWG in year '.$year.'</h1>";</script>';
   return $message;
 }
 add_shortcode('change_title', 'wpb_kgwg_change_title');
@@ -137,11 +172,12 @@ function wpb_kgwg_add_bootstrap() {
   // Register style & scripts
   //wp_register_style('kgwg-bootstrap-style', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
   //wp_register_script('kgwg-bootstarp-script', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', array(), '0.1', true);
-  wp_register_style('kgwg-bootstrap-style', '/wp-content/kgwg/kgwg_css/bootstrap.min.css');
-  wp_register_script('kgwg-bootstarp-script', '/wp-content/kgwg/kgwg_js/bootstrap.bundle.min.js', array(), '0.1', true);
+  //wp_register_style('kgwg-bootstrap-style', '/wp-content/kgwg/kgwg_css/bootstrap.min.css');
+  //wp_register_script('kgwg-bootstarp-script', '/wp-content/kgwg/kgwg_js/bootstrap.bundle.min.js', array(), '0.1', true);
+  wp_register_style('kgwg-bootstrap-style', '/wp-content/kgwg/kgwg_css/kgwg_styles.css');
 
   // Enqueue kgwg styles & scripts
   wp_enqueue_style('kgwg-bootstrap-style');
-  wp_enqueue_script('kgwg-bootstrap-script');
+  //wp_enqueue_script('kgwg-bootstrap-script');
 }
 add_action('wp_enqueue_scripts', 'wpb_kgwg_add_bootstrap');
